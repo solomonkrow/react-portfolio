65531713e49e67c1ef6aef86eb29ba1cac1f910c
solomonkrow <metro317@live.com>
2023-11-21 16:38:31 -0600

    starter code

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import NavBar from "./components/NavBar";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <NavBar />
+       
+      </div>
+      
+    </>
+  );
+}
+
+export default App
